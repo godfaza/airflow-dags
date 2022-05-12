e00b10e3299e6080c6fe39bc019501b731a03d92
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-12 15:28:25 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -15,11 +15,11 @@ with DAG(
     # [START howto_operator_bash]
     create_hdfs_config = BashOperator(
         task_id=create_hdfs_config',
-        bash_command="echo [global]
+        bash_command="""echo [global]
         default.alias = dev
 
         [dev.alias]
-          url = http://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net:9870 > ~/.hdfscli.cfg",
+          url = http://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net:9870 > ~/.hdfscli.cfg""",
     )
     dump_hdfs_config = BashOperator(
         task_id='dump_hdfs_config',
