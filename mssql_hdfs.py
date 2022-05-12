e3b63553dca1e3861d2b908a73a03ff477e7847e
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-12 17:09:40 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -15,7 +15,7 @@ with DAG(
     # [START howto_operator_bash]
     create_hdfs_config = BashOperator(
         task_id='create_hdfs_config',
-        bash_command="airflow info",
+        bash_command="ls /opt/airflow/logs",
             )
     dump_hdfs_config = BashOperator(
         task_id='dump_hdfs_config',
