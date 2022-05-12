e5670c7b32091cab0d01f6a2c14a2b8877f8046a
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-12 15:17:05 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -13,11 +13,23 @@ with DAG(
     catchup=False,
 ) as dag:
     # [START howto_operator_bash]
+    create_hdfs_config = BashOperator(
+        task_id='create_hdfs_config',
+        bash_command='echo "[global]
+        default.alias = dev
+
+        [dev.alias]
+          url = http://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net:9870" > ~/.hdfscli.cfg',
+    )
+    dump_hdfs_config = BashOperator(
+        task_id='dump_hdfs_config',
+        bash_command='cat ~/.hdfscli.cfg',
+    )  
     query_db = BashOperator(
         task_id='query_db',
         bash_command='/opt/mssql-tools18/bin/sqlcmd -S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1 -C -Q "SELECT ID,CODE FROM dbo.Country" -W -w 1024  -I',
     )
     
-    query_db
+    create_hdfs_config >> dump_hdfs_config >> query_db
    
   
