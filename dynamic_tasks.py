059c13167b8a405b7b6d3574882a864f46c3eda2
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-13 12:20:38 +0300

    Update dynamic_tasks.py

--- dynamic_tasks.py
+++ dynamic_tasks.py
@@ -1,6 +1,7 @@
 import datetime
-
 import pendulum
+import sys
+import hdfs
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -13,17 +14,33 @@ with DAG(
     catchup=False,
 ) as dag:
 
+  download_schema = BashOperator(
+        task_id='download_schema',
+        bash_command="cp -r /opt/airflow/logs/src/. ~/ && chmod +x ~/download_schema.sh && ~/download_schema.sh ",
+            )  
   start_op = BashOperator(
-        task_id='start_op',
-        bash_command='echo XSDSDSSD ',
-            )    
+        task_id='upload_schema',
+        bash_command='cp -r /opt/airflow/logs/src/. ~/ && ',
+            ) 
+    
+  cl=hdfs.client.Client(url="http://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net:9870")
+  src = "/user/smartadmin/schema/schema.csv"
+  dst = "~/schema.csv" 
+  print("from={} to={}".format(src,dst))
+  cl.download(src,dst)
+
+  with open(dst) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+  
+  print(lines)
   a = []
   for i in range(0,10):
     a.append(DummyOperator(
         task_id='Component'+str(i),
         dag=dag))
     if i == 0 :
-        start_op >> a[i]
+       download_schema >> start_op >> a[i]
     if i not in [0]: 
         a[i-1] >> a[i]
    
