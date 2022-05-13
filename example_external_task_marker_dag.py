0c3c05a417e047d5f6fc19767364c8356354425d
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-13 14:24:08 +0300

    Update example_external_task_marker_dag.py

--- example_external_task_marker_dag.py
+++ example_external_task_marker_dag.py
@@ -1,21 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
 """
 Example DAG demonstrating setting up inter-DAG dependencies using ExternalTaskSensor and
 ExternalTaskMarker
@@ -40,7 +22,7 @@ interval till one of the following will happen:
 import pendulum
 
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.dummy import DummyOperator
 from airflow.sensors.external_task import ExternalTaskMarker, ExternalTaskSensor
 
 start_date = pendulum.datetime(2021, 1, 1, tz="UTC")
@@ -78,5 +60,5 @@ with DAG(
         mode="reschedule",
     )
     # [END howto_operator_external_task_sensor]
-    child_task2 = EmptyOperator(task_id="child_task2")
+    child_task2 = DummyOperator(task_id="child_task2")
     child_task1 >> child_task2
