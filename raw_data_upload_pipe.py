fd8f4cd5f048033d577ae9731dfdc5879a00163a
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-13 12:40:38 +0300

    Update raw_data_upload_pipe.py

--- raw_data_upload_pipe.py
+++ raw_data_upload_pipe.py
@@ -23,7 +23,22 @@ def create_dag(dag_id,
 
     return dag
 
-
+def read_tables_list:
+  import sys
+  import hdfs
+  
+  cl=hdfs.client.Client(url="http://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net:9870")
+  src = "/user/smartadmin/schema/schema.csv"
+  dst = "~/schema.csv" 
+  print("from={} to={}".format(src,dst))
+  cl.download(src,dst)
+  
+   with open(dst) as file:
+    lines = file.readlines()
+    lines = [line.rstrip() for line in lines]
+  
+  print(lines)
+  
 # build a dag for each number in range(10)
 for n in range(1, 4):
     dag_id = 'raw_data_upload_pipe_{}'.format(str(n))
