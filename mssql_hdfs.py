023dc9a419925fc3013afcb1bdb375c58a61d6b4
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-16 10:13:04 +0300

    Update mssql_hdfs.py

--- mssql_hdfs.py
+++ mssql_hdfs.py
@@ -21,7 +21,7 @@ with DAG(
         task_id='download_table',
         bash_command="cp -r /opt/airflow/logs/src/. ~/ && chmod +x ~/download_table.sh && ~/download_table.sh {{params.table_name}} ",
         params = {'table_name':'YA_DATAMART4'},
-        dag=dag)
+        )
     query_db = BashOperator(
         task_id='query_db',
         bash_command='/opt/mssql-tools18/bin/sqlcmd -S 192.168.10.39 -d MIP_UtilizeOutbound_Main_Dev_Current -U userdb -P qwerty1 -C -Q "SELECT ID,CODE FROM dbo.Country" -W -w 1024  -I',
