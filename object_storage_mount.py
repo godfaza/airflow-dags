2a82b4d7b2a981ed57dff302fcf9da70d4bad992
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-16 14:02:53 +0300

    Create object_storage_mount.py

---
+++ object_storage_mount.py
@@ -0,0 +1,31 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+    dag_id='mssql_to_hdfs_export',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+
+    check_mount = BashOperator(
+        task_id='check_mount',
+        bash_command="ls -la /source-code",
+         executor_config={"KubernetesExecutor": {
+                                     "volume_mounts": [
+                                      {
+                                          "name": "airlow-source-code",
+                                          "mountPath": "/source-code"
+                                      },
+                                  }
+                             },
+    )
+
+    check_mount
+   
+  
