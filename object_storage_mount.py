ce1707b0cf11d5eb9f23a7c188d8b82d4f2f1876
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-18 16:30:18 +0300

    Update object_storage_mount.py

--- object_storage_mount.py
+++ object_storage_mount.py
@@ -21,7 +21,7 @@ with DAG(
     
     java_check = BashOperator(
         task_id='java_check',
-        bash_command="echo ABCDEFGxyz|hadoop fs -put - hdfs://rc1b-dataproc-m-3iu6zt2tusazxrxi.mdb.yandexcloud.net/user/smartadmin/data/out.txt",
+        bash_command="cp -r /tmp/data/src/. /etc/hadoop/ && echo ABCDEFGxyz|hadoop fs -put - hdfs://user/smartadmin/data/out11.txt",
    
     )
 
