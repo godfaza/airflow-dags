de4caed96e0def4e69b5c0137985633c15712ff2
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-25 17:18:36 +0300

    Update vault_secrets_test.py

--- vault_secrets_test.py
+++ vault_secrets_test.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.hooks.base import BaseHook
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 def print_var():
@@ -9,8 +10,13 @@ def print_var():
     print(f'My variable is: {my_var}')
 
 with DAG('vault_secrets_test', start_date=datetime(2022, 1, 1), schedule_interval=None) as dag:
-
+  ping = BashOperator(
+        task_id='ping',
+        bash_command="ping -c 3 jupiter-app-test-vault.ru-central1.internal",
+            )
   test_task = PythonOperator(
       task_id='test-task',
       python_callable=print_var,
 )
+
+  ping >>   test_task
