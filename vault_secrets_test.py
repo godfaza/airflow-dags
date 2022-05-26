41161fcbc6bcc83615596f5ce4a60b7f40e12e91
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-26 12:56:46 +0300

    Update vault_secrets_test.py

--- vault_secrets_test.py
+++ vault_secrets_test.py
@@ -10,13 +10,12 @@ def print_var():
     print(f'My variable is: {my_var}')
 
 with DAG('vault_secrets_test', start_date=datetime(2022, 1, 1), schedule_interval=None) as dag:
-#   ping = BashOperator(
-#         task_id='ping',
-#         bash_command="echo $AIRFLOW__SECRETS__BACKEND_KWARGS",
-#             )
+  ping = BashOperator(
+        task_id='ping',
+        bash_command="echo $AIRFLOW__SECRETS__BACKEND_KWARGS",
+            )
   test_task = PythonOperator(
       task_id='test-task',
       python_callable=print_var,
     )
-  
-  test_task
+
