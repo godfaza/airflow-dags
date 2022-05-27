e74b2571a7056ef34b4a3c497f01a3d473d57789
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-27 14:16:13 +0300

    Create bulk_import.py

---
+++ bulk_import.py
@@ -0,0 +1,23 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+with DAG(
+    dag_id='bulk_import',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+) as dag:
+
+    upload_file = BashOperator(
+        task_id='upload_file',
+        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import && hadoop dfs -cat /user/smartadmin/data/data.csv|~/bcp_import ",
+        )
+    
+    upload_file 
+   
+  
