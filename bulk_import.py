6e5efe069902d49c5600050abdcbe25614d602a1
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-27 14:55:06 +0300

    Update bulk_import.py

--- bulk_import.py
+++ bulk_import.py
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
     cat = BashOperator(
         task_id='cat',
-        bash_command=" ls -la /opt/microsoft/msodbcsql18/lib64 ",
+        bash_command="cat /etc/ld.so.conf ",
         )
     
     
