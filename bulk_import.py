49611c92c17e4a73fab99498d67e15447d8903e6
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-27 14:19:05 +0300

    Update bulk_import.py

--- bulk_import.py
+++ bulk_import.py
@@ -12,7 +12,6 @@ with DAG(
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
 ) as dag:
-
     upload_file = BashOperator(
         task_id='upload_file',
         bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/bcp_import && hadoop dfs -cat /user/smartadmin/data/data.csv|~/bcp_import ",
