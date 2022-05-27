db8d6c3a849bf1bce00dbc1d1960f30b1568954c
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-05-27 15:06:02 +0300

    Update bulk_import.py

--- bulk_import.py
+++ bulk_import.py
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
     cat = BashOperator(
         task_id='cat',
-        bash_command="cat /etc/ld.so.conf ",
+        bash_command="cat /etc/ld.so.conf.d/libc.conf ",
         )
     
     
