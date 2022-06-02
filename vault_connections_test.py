629a84e8efa231c1d63bc57fd50b3e8839bb3832
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-02 12:51:39 +0300

    Create vault_connections_test.py

---
+++ vault_connections_test.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+def print_var():
+    my_var = Variable.get("var999")
+    print(f'My variable is: {my_var}')
+
+with DAG('vault_connections_test', start_date=datetime(2022, 1, 1), schedule_interval=None) as dag:
+  ping = BashOperator(
+        task_id='ping',
+        bash_command="echo $AIRFLOW__SECRETS__BACKEND_KWARGS",
+            )
+  test_task = PythonOperator(
+      task_id='test-task',
+      python_callable=print_var,
+    )
