5a818508088fb6cd79aa02aeb32a96730e8118bc
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-02 14:31:10 +0300

    Update vault_connections_test.py

--- vault_connections_test.py
+++ vault_connections_test.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from airflow.hooks.base_hook import BaseHook
 
 def get_secrets(**kwargs):
-     conn = BaseHook.get_connection('default_email')
-     print(conn)
+     conn = BaseHook.get_connection(kwargs['my_conn_id'])
      print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}")
 
 with DAG('vault_connections_test', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
+
      test_task = PythonOperator(
          task_id='test-task',
          python_callable=get_secrets,
+         op_kwargs={'my_conn_id': 'smtp_default'},
      )
