a33ccf36cb69be9f8f5b38ce3395e70b38818aee
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-02 15:05:19 +0300

    Update vault_connections_test.py

--- vault_connections_test.py
+++ vault_connections_test.py
@@ -16,6 +16,6 @@ with DAG('vault_connections_test', start_date=datetime(2020, 1, 1), schedule_int
      )
      download_schema = BashOperator(
         task_id='exec_query',
-        bash_command="cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh \\"select 1\\" /user/smartadmin/schema/query_out.csv {{params.host}} {{params.schema}} {{params.login}} {{params.password}} ",
+        bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "select 1" /user/smartadmin/schema/query_out.csv {{params.host}} {{params.schema}} {{params.login}} {{params.password}} ',
         params = BaseHook.get_connection('jupiter_dev_mssql'),  
             )
