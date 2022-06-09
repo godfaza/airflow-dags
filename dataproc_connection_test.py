de52a20c4eb889d46e2bd8f78085538c9dbdeca2
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-09 17:06:55 +0300

    Create dataproc_connection_test.py

---
+++ dataproc_connection_test.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.hooks.base_hook import BaseHook
+
+def get_secrets(**kwargs):
+     conn = BaseHook.get_connection(kwargs['my_conn_id'])
+     print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}, Schema: {conn.schema}")
+     return '-S {} -d {} -U {} -P {}'.format(conn.host,conn.schema,conn.login,conn.password)
+
+with DAG('dataproc_connection_test', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
+
+     test_task = PythonOperator(
+         task_id='test-task',
+         python_callable=get_secrets,
+         op_kwargs={'my_conn_id': 'yandexcloud_default'},
+     )
+     test_task 
