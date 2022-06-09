3440a6f2c618c1781285ca4d3d18a5b63309078d
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-09 17:13:49 +0300

    Update dataproc_connection_test.py

--- dataproc_connection_test.py
+++ dataproc_connection_test.py
@@ -6,7 +6,7 @@ from airflow.hooks.base_hook import BaseHook
 
 def get_secrets(**kwargs):
      conn = BaseHook.get_connection(kwargs['my_conn_id'])
-     print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}, Schema: {conn.schema}")
+     print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}, Schema: {conn.schema},  Extra: {conn.extra}")
      return '-S {} -d {} -U {} -P {}'.format(conn.host,conn.schema,conn.login,conn.password)
 
 with DAG('dataproc_connection_test', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
