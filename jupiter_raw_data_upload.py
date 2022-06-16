c7cf754fda999fc7ee1ad2625af3435e460de30b
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-16 16:20:33 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -45,7 +45,7 @@ with DAG(
   extract_db_schema = PythonOperator(
       task_id='extract_db_schema',
       python_callable=get_db_schema,
-      op_kwargs={'white_list':'dbo.MANUAL;dbo.YA_DATAMART_DELTA'},
+      op_kwargs={'white_list':{{ti.xcom_pull(task_ids="get_parameters_from_kv")["WhiteList"]}},
     )
   get_parameters_from_kv >>  extract_db_schema 
 
