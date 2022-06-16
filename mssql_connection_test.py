1a621c1c8b953b2b9c58d175336a5b79f3764e66
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-16 09:37:05 +0300

    Update mssql_connection_test.py

--- mssql_connection_test.py
+++ mssql_connection_test.py
@@ -14,15 +14,10 @@ with DAG(
     tags=['mssql_test'],
     catchup=False,
 ) as dag:
-#     query1 = MsSqlOperator(
-#         task_id="query1",
-#         mssql_conn_id='jupiter_dev_mssql',
-#         sql=r"""SELECT * FROM Country;""",
-#     )
-    echo = BashOperator(
-        task_id='exec_query',
-          bash_command='echo 1234',
-            )
-    
-    echo
+    query1 = MsSqlOperator(
+        task_id="query1",
+        mssql_conn_id='jupiter_dev_mssql',
+        sql=r"""SELECT * FROM Country;""",
+    )
+
  
