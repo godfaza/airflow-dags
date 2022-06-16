995993501ab3c744d99bfca28c79ae0f97bfbfa9
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-16 14:18:16 +0300

    Create jupiter_raw_data_upload.py

---
+++ jupiter_raw_data_upload.py
@@ -0,0 +1,39 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.task_group import TaskGroup
+import uuid
+from airflow.providers.yandex.operators.yandexcloud_dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocCreateHiveJobOperator,
+    DataprocCreateMapReduceJobOperator,
+    DataprocCreatePysparkJobOperator,
+    DataprocCreateSparkJobOperator,
+    DataprocDeleteClusterOperator,
+)
+
+AVAILABILITY_ZONE_ID = 'ru-central1-b'
+S3_BUCKET_NAME_FOR_JOB_LOGS = 'jupiter-app-test-storage'
+
+def get_parameters():
+    my_var = Variable.get("var999")
+    print(f'My variable is: {my_var}')
+
+with DAG(
+    dag_id='jupiter_raw_data_upload',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["jupiter","dev"],
+) as dag:
+  get_parameters_from_kv = PythonOperator(
+      task_id='get_parameters_from_kv',
+      python_callable=get_parameters,
+    ) 
+
+
+   
+  
