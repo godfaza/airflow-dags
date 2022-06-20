3761a27b24c4a0d9a2e0cc85d5323fdb6c32a172
AlekseyLoktev83 <alekseyloktev83@gmail.com>
2022-06-20 12:59:33 +0300

    2

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -21,6 +21,7 @@ from airflow.providers.yandex.operators.yandexcloud_dataproc import (
     DataprocDeleteClusterOperator,
 )
 import cloud_scripts.mssql_scripts as mssql_scripts
+import json
 
 AVAILABILITY_ZONE_ID = 'ru-central1-b'
 S3_BUCKET_NAME_FOR_JOB_LOGS = 'jupiter-app-test-storage'
