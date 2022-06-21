371b0ea7b2e314c53870c48e54a44d66d61b969f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-21 11:59:52 +0300

    Update dynamic_task_mapping.py

--- dynamic_task_mapping.py
+++ dynamic_task_mapping.py
@@ -1,15 +1,16 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
+from airflow.decorators import task
 
 import random
 
 with DAG(dag_id='dynamic_task_mapping_example', start_date=days_ago(2), catchup=False) as dag:
 
-    @dag.task
+    @task
     def make_list():
         return [i + 1 for i in range(random.randint(2, 4))]
 
-    @dag.task
+    @task
     def consumer(value):
         print(repr(value))
 
