3a0debbb501d1ed67e1c6c3aae781299128f7e40
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-21 11:48:14 +0300

    Update dynamic_task_mapping.py

--- dynamic_task_mapping.py
+++ dynamic_task_mapping.py
@@ -7,12 +7,13 @@ from airflow.decorators import task
 with DAG(dag_id="simple_mapping", start_date=datetime(2022, 3, 4)) as dag:
 
     @task
-    def add_one(x: int):
+    def add_one(x):
         return x + 1
 
     @task
     def sum_it(values):
         total = sum(values)
+        
         print(f"Total was {total}")
 
     added_values = add_one.expand(x=[1, 2, 3])
