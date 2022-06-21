1ce2ad398162f8fbc306038dd9dbf45eb30ff58a
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-21 12:13:09 +0300

    Update dynamic_task_mapping.py

--- dynamic_task_mapping.py
+++ dynamic_task_mapping.py
@@ -1,38 +1,20 @@
-import time
-
-from airflow.decorators import task, dag
 from datetime import datetime
-from pathlib import Path
+
+from airflow import DAG
+from airflow.decorators import task
 
 
-logs_file = Path("/data/dummy_logs/random_logs.log")
+with DAG(dag_id="simple_mapping", start_date=datetime(2022, 3, 4)) as dag:
 
+    @task
+    def add_one(x):
+        return x + 1
 
-def get_process_id(line: str) -> str:
-   return line.split(" ")[-1]
+    @task
+    def sum_it(values):
+        total = sum(values)
+        
+        print(f"Total was {total}")
 
-
-@dag(start_date=datetime(2022, 5, 16),
-    schedule_interval="@daily",
-    dag_id="repair_components_dummy",
-    catchup=False)
-def my_dag():
-   @task
-   def get_failed_processes(log_file: Path) -> list[str]:
-       failed_processes = []
-
-       with open(log_file, 'r') as f:
-           for line in f:
-               if line.startswith("ERROR"):
-                   failed_processes.append(get_process_id(line))
-
-       return failed_processes
-
-   @task
-   def repair_process(process_id: str):
-       time.sleep(0.5)  # Simulate computation time
-       print(f"process {process_id} repaired")
-
-   repair_process.expand(process_id=get_failed_processes(logs_file))
-
-repair_logs_dummy = my_dag()
+    added_values = add_one.expand(x=[1, 2, 3])
+    sum_it(added_values)
