dff89458bc16a9d0b7c7b8709434e84496b1d08a
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-22 11:22:18 +0300

    Update dynamic_task_mapping.py

--- dynamic_task_mapping.py
+++ dynamic_task_mapping.py
@@ -6,32 +6,22 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.decorators import dag, task
 
-# @task
-# def make_list():
-#     # This can also be from an API call, checking a database, -- almost anything you like, as long as the
-#     # resulting list/dictionary can be stored in the current XCom backend.
-#     return ["echo DS1","echo DS2","echo DS3"]
+@task
+def make_list():
+    return ["echo DS1","echo DS2","echo DS3"]
 
 
 @task
 def consumer(arg):
     print(list(arg))
-    
-def _make_list():
-    return ["echo DS1","echo DS2","echo DS3"]    
 
 
 with DAG(dag_id="dynamic-map", 
          start_date=datetime(2022, 4, 2),
          schedule_interval=None,
         ) as dag:
-    
-      make_list = PythonOperator(
-        task_id='make_list',
-        python_callable=_make_list,
-        provide_context=True,
-    )
-      echo_op=BashOperator.partial(task_id="bash", do_xcom_push=False).expand(
+
+    echo_op=BashOperator.partial(task_id="bash", do_xcom_push=False).expand(
        bash_command=dag.make_list(),
     )
 #     consumer.expand(arg=make_list())
