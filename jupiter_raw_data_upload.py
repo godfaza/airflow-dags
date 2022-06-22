5fe216856c1608d9e2a802c8296bb55cb9079e87
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-22 12:06:13 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -45,10 +45,8 @@ def get_parameters(**kwargs):
     return parameters
 
 
-def _extract_db_schema(**context):
-    parameters = context['ti'].xcom_pull(task_ids="get_parameters")
-    dst_path = "{}/{}".format(parameters['RawPath'],
-                              "/#MAINTENANCE/PARAMETERS.csv")
+def extract_db_schema(parameters):
+    dst_path = "{}/{}".format(parameters['MaintenancePath'],"PARAMETERS.csv")
     query = mssql_scripts.generate_db_schema_query(
         white_list=parameters['WhiteList'])
     return query
@@ -63,32 +61,32 @@ def _extract_db_schema(**context):
 
 #     conn.upload(dst_path,'/tmp/PARAMETERS.csv')
 
-def _generate_upload_scripts(**context):
-    parameters = context['ti'].xcom_pull(task_ids="get_parameters")
-    src_path = context['ti'].xcom_pull(task_ids="get_parameters",key="MaintenancePath")+"EXTRACT_ENTITIES_AUTO.csv"
-    print(src_path)
-    hdfs_hook = WebHDFSHook()
-    conn = hdfs_hook.get_conn()
-    conn.download(src_path, '/tmp/PARAMETERS.csv')
+# def _generate_upload_scripts(**context):
+#     parameters = context['ti'].xcom_pull(task_ids="get_parameters")
+#     src_path = context['ti'].xcom_pull(task_ids="get_parameters",key="MaintenancePath")+"EXTRACT_ENTITIES_AUTO.csv"
+#     print(src_path)
+#     hdfs_hook = WebHDFSHook()
+#     conn = hdfs_hook.get_conn()
+#     conn.download(src_path, '/tmp/PARAMETERS.csv')
     
-    out_query = mssql_scripts.generate_table_select_query('2022-06-20','2022-06-20','/tmp/PARAMETERS.csv')
-    print(out_query)
-    return  out_query
+#     out_query = mssql_scripts.generate_table_select_query('2022-06-20','2022-06-20','/tmp/PARAMETERS.csv')
+#     print(out_query)
+#     return  out_query
 
-def _iterate_upload_scripts(**context):
-    parameters = context['ti'].xcom_pull(task_ids="get_parameters")
-    df = pd.read_csv(StringIO(context['ti'].xcom_pull(task_ids="generate_upload_scripts")),keep_default_na=False)
-    df = df.reset_index()
-    bcp_parameters = context['ti'].xcom_pull(task_ids="get_bcp_parameters")
+# def _iterate_upload_scripts(**context):
+#     parameters = context['ti'].xcom_pull(task_ids="get_parameters")
+#     df = pd.read_csv(StringIO(context['ti'].xcom_pull(task_ids="generate_upload_scripts")),keep_default_na=False)
+#     df = df.reset_index()
+#     bcp_parameters = context['ti'].xcom_pull(task_ids="get_bcp_parameters")
 
-    for index, row in df.iterrows():
-     print(row['EntityName'], row['Extraction'])
-     dst_path = "{}/{}.csv".format(parameters["RawPath"],row['EntityName'])
-     command = 'cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{query}" {dst_path} "{bcp_parameters}" '.format(query=row['Extraction'],dst_path=dst_path,bcp_parameters=bcp_parameters)
-     print(command)
-     ret = subprocess.run(command, capture_output=True, shell=True)
-     print(ret)
-#      print('#######################-----------------------#######################')
+#     for index, row in df.iterrows():
+#      print(row['EntityName'], row['Extraction'])
+#      dst_path = "{}/{}.csv".format(parameters["RawPath"],row['EntityName'])
+#      command = 'cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{query}" {dst_path} "{parameters.output["BcpParameters"]}" '.format(query=row['Extraction'],dst_path=dst_path,bcp_parameters=bcp_parameters)
+#      print(command)
+#      ret = subprocess.run(command, capture_output=True, shell=True)
+#      print(ret)
+# #      print('#######################-----------------------#######################')
 
 
 with DAG(
@@ -101,26 +99,28 @@ with DAG(
 ) as dag:
     
     parameters = get_parameters()
-    extract_db_schema = PythonOperator(
-        task_id='extract_db_schema',
-        python_callable=_extract_db_schema,
-        provide_context=True,
-    )
-    save_db_schema = BashOperator(
-        task_id='save_db_schema',
-        #           bash_command='echo "{{ ti.xcom_pull(task_ids="test-task") }}"',
-        bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{{ti.xcom_pull(task_ids="extract_db_schema")}}" {{ti.xcom_pull(task_ids="get_parameters",key="MaintenancePath")}}EXTRACT_ENTITIES_AUTO.csv "{{ti.xcom_pull(task_ids="get_bcp_parameters")}}" "Schema,TableName,FieldName,Position,FieldType,Size,IsNull,UpdateDate,Scale"',
-    )
+    db_schema = extract_db_schema(parameters)
+    
+#     extract_db_schema = PythonOperator(
+#         task_id='extract_db_schema',
+#         python_callable=_extract_db_schema,
+#         provide_context=True,
+#     )
+#     save_db_schema = BashOperator(
+#         task_id='save_db_schema',
+#         #           bash_command='echo "{{ ti.xcom_pull(task_ids="test-task") }}"',
+#         bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{{ti.xcom_pull(task_ids="extract_db_schema")}}" {{ti.xcom_pull(task_ids="get_parameters",key="MaintenancePath")}}EXTRACT_ENTITIES_AUTO.csv "{{ti.xcom_pull(task_ids="get_bcp_parameters")}}" "Schema,TableName,FieldName,Position,FieldType,Size,IsNull,UpdateDate,Scale"',
+#     )
 
-    generate_upload_scripts = PythonOperator(
-        task_id='generate_upload_scripts',
-        python_callable=_generate_upload_scripts,
-        provide_context=True,
-    )
-    iterate_upload_scripts = PythonOperator(
-        task_id='iterate_upload_scripts',
-        python_callable=_iterate_upload_scripts,
-        provide_context=True,
-    )
+#     generate_upload_scripts = PythonOperator(
+#         task_id='generate_upload_scripts',
+#         python_callable=_generate_upload_scripts,
+#         provide_context=True,
+#     )
+#     iterate_upload_scripts = PythonOperator(
+#         task_id='iterate_upload_scripts',
+#         python_callable=_iterate_upload_scripts,
+#         provide_context=True,
+#     )
 
-    parameters >> extract_db_schema >> save_db_schema >> generate_upload_scripts >> iterate_upload_scripts
+#     parameters >> extract_db_schema >> save_db_schema >> generate_upload_scripts >> iterate_upload_scripts
