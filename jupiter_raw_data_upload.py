740a5afece073e6337830b6912cb1d5f26f83660
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-22 14:36:55 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -104,7 +104,7 @@ with DAG(
     
     parameters = get_parameters()
     schema_query = generate_schema_query(parameters)
-    copy_data_db_to_hdfs(schema_query,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv")
+    extract_schema = copy_data_db_to_hdfs(schema_query,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv")
     generate_upload_scripts(parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv")                                                      
     
 #     extract_db_schema = PythonOperator(
