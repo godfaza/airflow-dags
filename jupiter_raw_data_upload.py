2ef24dac89a24d0986fa119a40077662cb0ded0f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-23 10:37:15 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -89,22 +89,6 @@ def generate_upload_scripts(prev_task,src_dir,src_file,upload_path,bcp_parameter
     print(scripts_list)
     return  scripts_list
 
-# def _iterate_upload_scripts(**context):
-#     parameters = context['ti'].xcom_pull(task_ids="get_parameters")
-#     df = pd.read_csv(StringIO(context['ti'].xcom_pull(task_ids="generate_upload_scripts")),keep_default_na=False)
-#     df = df.reset_index()
-#     bcp_parameters = context['ti'].xcom_pull(task_ids="get_bcp_parameters")
-
-#     for index, row in df.iterrows():
-#      print(row['EntityName'], row['Extraction'])
-#      dst_path = "{}/{}.csv".format(parameters["RawPath"],row['EntityName'])
-#      command = 'cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{query}" {dst_path} "{parameters.output["BcpParameters"]}" '.format(query=row['Extraction'],dst_path=dst_path,bcp_parameters=bcp_parameters)
-#      print(command)
-#      ret = subprocess.run(command, capture_output=True, shell=True)
-#      print(ret)
-# #      print('#######################-----------------------#######################')
-
-
 with DAG(
     dag_id='jupiter_raw_data_upload',
     schedule_interval=None,
@@ -117,30 +101,6 @@ with DAG(
     parameters = get_parameters()
     schema_query = generate_schema_query(parameters)
     extract_schema = copy_data_db_to_hdfs(schema_query,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv")
-#     generate_upload_scripts(extract_schema,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv",parameters["UploadPath"],parameters["BcpParameters"])                                                      
     upload_tables=BashOperator.partial(task_id="upload_tables", do_xcom_push=False).expand(
        bash_command=generate_upload_scripts(extract_schema,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv",parameters["UploadPath"],parameters["BcpParameters"])  ,
     )
-#     extract_db_schema = PythonOperator(
-#         task_id='extract_db_schema',
-#         python_callable=_extract_db_schema,
-#         provide_context=True,
-#     )
-#     save_db_schema = BashOperator(
-#         task_id='save_db_schema',
-#         #           bash_command='echo "{{ ti.xcom_pull(task_ids="test-task") }}"',
-#         bash_command='cp -r /tmp/data/src/. ~/ && chmod +x ~/exec_query.sh && ~/exec_query.sh "{{ti.xcom_pull(task_ids="extract_db_schema")}}" {{ti.xcom_pull(task_ids="get_parameters",key="MaintenancePath")}}EXTRACT_ENTITIES_AUTO.csv "{{ti.xcom_pull(task_ids="get_bcp_parameters")}}" "Schema,TableName,FieldName,Position,FieldType,Size,IsNull,UpdateDate,Scale"',
-#     )
-
-#     generate_upload_scripts = PythonOperator(
-#         task_id='generate_upload_scripts',
-#         python_callable=_generate_upload_scripts,
-#         provide_context=True,
-#     )
-#     iterate_upload_scripts = PythonOperator(
-#         task_id='iterate_upload_scripts',
-#         python_callable=_iterate_upload_scripts,
-#         provide_context=True,
-#     )
-
-#     parameters >> extract_db_schema >> save_db_schema >> generate_upload_scripts >> iterate_upload_scripts
