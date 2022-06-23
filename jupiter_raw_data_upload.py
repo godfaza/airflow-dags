d13aaf02ad3d61bc920ff62c5515d2d87b0c4827
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-23 15:18:50 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -115,4 +115,6 @@ with DAG(
     upload_tables=BashOperator.partial(task_id="upload_tables", do_xcom_push=True).expand(
        bash_command=generate_upload_scripts(extract_schema,parameters["MaintenancePath"],"EXTRACT_ENTITIES_AUTO.csv",parameters["UploadPath"],parameters["BcpParameters"])  ,
     )
-    monitoring_results = save_monitoring_result.expand(input=XComArg(upload_tables))
+    
+#     monitoring_results = save_monitoring_result.expand(input=XComArg(upload_tables))
+    monitoring_results = save_monitoring_result(upload_tables)
