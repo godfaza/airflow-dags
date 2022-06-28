67296938cf7a5245919b6cfa16a312eb4475a616
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-28 12:49:43 +0300

    Update jupiter_raw_data_upload.py

--- jupiter_raw_data_upload.py
+++ jupiter_raw_data_upload.py
@@ -158,7 +158,7 @@ def start_monitoring_detail(dst_dir,upload_path,input,run_id=None):
     
     return input
 
-
+@task
 def end_monitoring_detail(dst_dir,input):
     prev_tast_output = json.loads(input)
     
