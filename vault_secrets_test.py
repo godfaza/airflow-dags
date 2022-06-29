75e7bf814386032f2aa4116d334d1a6a94a7920f
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-29 09:18:28 +0300

    Update vault_secrets_test.py

--- vault_secrets_test.py
+++ vault_secrets_test.py
@@ -6,15 +6,19 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime
 from airflow.configuration import ensure_secrets_loaded
 from airflow.providers.hashicorp.secrets.vault import VaultBackend
+from airflow.providers.hashicorp.hooks.vault import VaultHook
 
 def print_var():
     my_var = Variable.get("var999")
     print(f'My variable is: {my_var}')
-    for secrets_backend in ensure_secrets_loaded():
-      if isinstance(secrets_backend, VaultBackend):
-        print(secrets_backend.vault_client.mount_point)
-        secrets_backend.vault_client.client.secrets.kv.v1.create_or_update_secret(path="variables/var999",secret={"value":"AIRFLOW_UPD"})
-#     Variable.update("var999", "airflow_update")
+    
+    vault_hook = VaultHook()
+    conn = vault_hook.get_conn()
+    conn.create_or_update_secret(path="variables/var999",secret={"value":"AIRFLOW_UPD1"})
+#     for secrets_backend in ensure_secrets_loaded():
+#       if isinstance(secrets_backend, VaultBackend):
+#         print(secrets_backend.vault_client.mount_point)
+#         secrets_backend.vault_client.client.secrets.kv.v1.create_or_update_secret(path="variables/var999",secret={"value":"AIRFLOW_UPD"})
     
 
 with DAG('vault_secrets_test', start_date=datetime(2022, 1, 1), schedule_interval=None) as dag:
