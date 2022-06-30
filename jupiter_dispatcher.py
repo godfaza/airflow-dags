c62a1444a683b58813e9eae239f49d0a68f71173
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-30 13:17:17 +0300

    Update jupiter_dispatcher.py

--- jupiter_dispatcher.py
+++ jupiter_dispatcher.py
@@ -15,7 +15,7 @@ with DAG(
     trigger_dag_qc = TriggerDagRunOperator(
         task_id="trigger_raw_qc",
         trigger_dag_id="jupiter_raw_qc",  
-        conf={"message": "Starting child dag"},
+#         conf={"message": "Starting child dag"},
         wait_for_completion = True,
     )
     
