71e29bdc3f45334c20d8b1c11a1f84d960b3bfb6
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-30 16:24:28 +0300

    Update dataproc_test.py

--- dataproc_test.py
+++ dataproc_test.py
@@ -29,7 +29,7 @@ with DAG(
     create_pyspark_job = DataprocCreatePysparkJobOperator(
         task_id='create_pyspark_job',
         cluster_id='c9qc9m3jccl8v7vigq10',
-       main_python_file_uri='/user/hive/warehouse/src',
+       main_python_file_uri='/user/hive/warehouse/src/JUPITER_ROLLING_VOLUMES_HDFS_FDM.py',
 #         main_python_file_uri='s3a://jupiter-app-test-storage/src/JUPITER_ROLLING_VOLUMES_HDFS_FDM.py',
         file_uris=[
             's3a://data-proc-public/jobs/sources/data/config.json',
