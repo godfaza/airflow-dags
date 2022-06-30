269857cdb2eae1dae88e15d71b8ca7b1bfe1eb11
AlekseyLoktev83 <96654944+AlekseyLoktev83@users.noreply.github.com>
2022-06-30 13:13:52 +0300

    Create jupiter_dispatcher.py

---
+++ jupiter_dispatcher.py
@@ -0,0 +1,22 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="jupiter_dispatcher",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    schedule_interval=None,
+    tags=["jupiter", "dev"],
+) as dag:
+   
+    trigger_dag_qc = TriggerDagRunOperator(
+        task_id="trigger_raw_qc",
+        trigger_dag_id="jupiter_raw_qc",  
+        conf={"message": "Starting child dag"},
+        wait_for_completion = True,
+    )
+    
+    trigger_dag_qc
